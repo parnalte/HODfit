90a899cde04b25cd291652adcdedba3a00b48fe9
Pablo Arnalte-Mur <pablo.arnalte-mur@durham.ac.uk>
2015-01-17 20:39:01 +0100

    Make transformation Pk --> xir using available 'hankel.py' library
    Change the way in which Pk --> xir transform is implemented. To do this
    I use the 'hankel.py' library by Steven Murray
    (https://pypi.python.org/pypi/hankel), more or less similar to what was
    done in CosmoPy, but readily available.
    I also find the parameters to make the transform fast and accurate.

--- simple_model/hods_utils.py
+++ simple_model/hods_utils.py
@@ -15,6 +15,8 @@
 import numpy as np
 import astropy.cosmology as ac
 import hankel
+import contextlib
+import sys
 
 ##GLOBAL CONSTANTS
 
@@ -26,7 +28,77 @@ import hankel
 RHO_CRIT_UNITS = 2.7752E+11
 
 
+#Define class and context to be able to 'silence' the output from
+#some part of the code or specific funtion
+#Taken from http://stackoverflow.com/a/2829036/2903411
 
+class DummyFile(object):
+    """
+    Dummy file object to implement http://stackoverflow.com/a/2829036/2903411
+    """
+    def write(self, x): pass
+
+@contextlib.contextmanager
+def nostdout():
+    """
+    New 'no output' context to implement http://stackoverflow.com/a/2829036/2903411
+    """
+    save_stdout = sys.stdout
+    sys.stdout = DummyFile()
+    yield
+    sys.stdout = save_stdout
+
+
+
+
+def aux_pkint_hankel(y_kr, r, pkfunction):
+    """
+    Auxiliar function to use in pk2xir_hankel. This is the function in the integrand, expressed
+    in terms of y=kr (this is the variable over which we perform the integral), and r.
+    pkfunction is a function that returns the power spectrum P(k) (can be an interpolant over
+    tabulated values)
+    """
+
+    return (y_kr**2)*pkfunction(y_kr/r)
+
+    
+    
+#These values of hankelN, hankelh = (6000, 0.0005) seem to work very well over all the
+#relevant scales using WMAP7_z0_lin, probably fine for all our purposes
+def pk2xir_hankel(rvalues, pkfunction, hankelN=6000, hankelh=0.0005):
+    """
+    Function that makes the standard tranformation from P(k) to xi(r) performing a
+    Hankel transform, using the functions in library 'hankel.py' by Steven Murray.
+
+    We convert the standard transformation, given by
+
+    \\xi(r) = \int_0^{+\infty} P(k) \frac{\sin(kr)}{kr} \frac{4 \pi k^2}{(2\pi)^3} dk ,
+
+    by making the change of variables y = kr, which results in
+
+    \\xi(r) = \frac{4\pi}{(2 \pi r)^3} \int_0^{+\infty} [y^2 P(y/r)] j_0(y) dy ,
+
+    where j_0(y) = sin(y)/y
+    """
+
+    Nr = len(rvalues)
+
+    xi_out = np.empty(Nr,float)
+
+    #Define the needed Hankel Transform instance, avoid printing
+    #lots of stuff coming from the definition
+    with nostdout():
+        sph_hankel = hankel.SphericalHankelTransform(nu=0, N=hankelN, h=hankelh)
+    
+    for i,r in enumerate(rvalues):
+        func_int = lambda x: aux_pkint_hankel(y_kr=x, r=r, pkfunction=pkfunction)
+        norm = 1./(2.*pow(np.pi, 2)*pow(r,3.))
+        int_result = sph_hankel.transform(func_int)[0]
+        xi_out[i] = norm*int_result
+
+    return xi_out
+
+    
 class PowerSpectrum:
     """Simple class to contain a power spectrum sampled at a set of
        given values of the wavenumber k
@@ -69,16 +141,18 @@ class PowerSpectrum:
         return np.interp(x=kval, xp=self.k, fp=self.pk)
 
 
-    def xir(self, rvals):
-        """ Function that performs a Hankel transform to obtain the 2-point correlation function
-            corresponding to the power spectrum.
-            Using the Hankel library from CosmoPy.
+
+    def xir(self, rvals, hankelN=6000, hankelh=0.0005):
+        """
+        Function that performs a Hankel transform to obtain the 2-point correlation function
+        corresponding to the power spectrum.
         """
 
-        hankel_instance = hankel.Hankel(dim=3)
-        xivals = hankel_instance.transform(f=self.pkinterp, x=rvals, pk2xi=True)
+        xivals = pk2xir_hankel(rvalues=rvals, pkfunction=self.pkinterp,
+                               hankelN=hankelN, hankelh=hankelh)
 
         return xivals
 
+    
         
         
