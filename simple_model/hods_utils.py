7053eb19a7c35f3a9d953a1c29aadc861876c9ba
Pablo Arnalte-Mur <pablo.arnalte@uv.es>
2015-01-19 15:10:47 +0100

    Created functions to transform an isotropic xi(r) to projected wp(rp)

--- simple_model/hods_utils.py
+++ simple_model/hods_utils.py
@@ -14,6 +14,7 @@
 
 import numpy as np
 import astropy.cosmology as ac
+from scipy import integrate
 import hankel
 import contextlib
 import sys
@@ -81,6 +82,8 @@ def pk2xir_hankel(rvalues, pkfunction, hankelN=6000, hankelh=0.0005):
     where j_0(y) = sin(y)/y
     """
 
+    rvalues = np.atleast_1d(rvalues)
+    assert rvalues.ndim == 1
     Nr = len(rvalues)
 
     xi_out = np.empty(Nr,float)
@@ -98,6 +101,80 @@ def pk2xir_hankel(rvalues, pkfunction, hankelN=6000, hankelh=0.0005):
 
     return xi_out
 
+
+
+def xi2d_iso(rp, pi, xirfunc):
+
+    rval = np.sqrt((rp*rp) + (pi*pi))
+
+    return xirfunc(rval)
+    
+
+def xir2wp_xifunc_pi(rpvals, xirfunc, logpimin=-4, logpimax=3, npi=500):
+    """
+    Get the value of the projected correlation function wp at a projected
+    separation rp from a xi(r) defined by function 'xirfunc'.
+
+    We assume isotropy and integrate along the pi direction (using a
+    log-space pi array defined by the corresponding parameters).
+    This is better than using the direct r-->rp conversion (e.g. eq. 2.3
+    in thesis), as we avoid having a divergence at r=rp.
+
+    Will use Simpson's integration along the pi direction, doing the
+    integration from -pi_max to pi_max (i.e. taking also negative pi)
+    in order to better sample the region around pi=0.
+    
+    rp can be an array (although for the moment we deal with it in
+    the slow non-pythonic way)
+    """
+
+
+    #First, checks on rp and convert to 1D array
+    rpvals = np.atleast_1d(rpvals)
+    assert rpvals.ndim == 1
+    Nrp = len(rpvals)
+
+    wp_out = np.empty(Nrp, float)
+
+    piarr = np.logspace(logpimin, logpimax, npi)
+    
+    #Start loop over rpvalues
+    for i,rp in enumerate(rpvals):
+
+        #Get values of xi
+        xi2d_arr = xi2d_iso(rp=rp, pi=piarr, xirfunc=xirfunc)
+
+        #Construct arrays for both negative and positive pi
+        piarr_full = np.concatenate((-piarr[::-1], piarr))
+        xi2darr_full = np.concatenate((xi2d_arr[::-1], xi2d_arr))
+
+        #Do the actual integration
+        wp_out[i] = integrate.simps(y=xi2darr_full, x=piarr_full)
+        
+    return wp_out
+
+
+
+def xir2wp_pi(rpvals, rvals, xivals, logpimin=-4, logpimax=3, npi=500):
+    """
+    Wrapper over the function xir2wp_xifunc_pi for the case in which xi(r)
+    is defined by a set of tabulated values. We just create a linear interpolator
+    of it and pass it to the function.
+    """
+    
+    #Now, checks on tabulated values of xi(r)
+    if not (sorted(rvals) == rvals).all():
+        raise UserWarning("In function xir2wp_pi: the values of r you gave me were not sorted!")
+
+        idx_sort = rvals.argsort()
+        rvals = rvals[idx_sort]
+        xivals = xivals[idx_sort]
+
+    xifunc = lambda x: np.interp(x, rvals, xivals)
+
+    return xir2wp_xifunc_pi(rpvals=rpvals, xirfunc=xifunc, logpimin=logpimin, logpimax=logpimax, npi=npi)
+    
+    
     
 class PowerSpectrum:
     """Simple class to contain a power spectrum sampled at a set of
