d18b4ded28f316c81391bc8980e8bcb55e155c22
Pablo Arnalte-Mur <pablo.arnalte@uv.es>
2015-02-17 10:48:28 +0100

    Added comparison to CosmoPMC results to 'test' directory

---
+++ full_model/test/manual_test_comparison.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import hods_clustering as hc
+import numpy as np
+import pylab as pl
+
+hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762)
+
+rp = np.logspace(-2,2,30)
+
+print "Computing default model"
+wp = hc.get_wptotal(rpvals=rp, clustering_object=hcobj)
+
+#With constant bias
+hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, scale_dep_bias=False)
+
+print "Computing constant bias model"
+wp_cbias = hc.get_wptotal(rpvals=rp, clustering_object=hcobj)
+
+#Without Mvir limit in cs term
+hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, use_mvir_limit=False)
+
+print "Computing no-Mvir-lim model"
+wp_nomvirlim = hc.get_wptotal(rpvals=rp, clustering_object=hcobj)
+
+#Without halo exclusion in 2h term
+hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, halo_exclusion_model=0)
+
+print "Computing no halo exclusion model"
+wp_nohaloexcl = hc.get_wptotal(rpvals=rp, clustering_object=hcobj)
+
+
+
+rpcp, wpcp = np.loadtxt("haloplot_wp.out", usecols=range(2), unpack=1)
+
+pl.plot(rpcp, wpcp, 'k-', lw=3, label="CosmoPMC")
+pl.plot(rp,wp,'o-', lw=2, label="My code - Present full model")
+pl.plot(rp,wp_cbias, '+-', label="My code - Constant bias")
+pl.plot(rp,wp_nomvirlim, '+-', label="My code - No Mvir limit in CS term")
+pl.plot(rp,wp_nohaloexcl, '+-', label="My code - No halo exclusion in 2h term")
+
+
+
+pl.loglog()
+pl.legend(loc=0)
+pl.xlabel(r'$r_p$')
+pl.ylabel(r'$w_p$')
+pl.show()
