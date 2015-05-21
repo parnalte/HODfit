81d4b6d7c9d9ce5412e35e52877df6f0812aabf1
Pablo Arnalte-Mur <pablo.arnalte@uv.es>
2015-05-21 16:20:26 +0200

    Added basic setup.py to be able to install using setuptools

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Basic setup for package hodfit.
+Basis taken from http://www.scotttorborg.com/python-packaging/minimal.html
+
+Created on Thu May 21 15:57:59 2015
+@author: pablo
+"""
+
+from setuptools import setup
+
+setup(name='hodfit',
+      version='0.4',
+      description='Fit HOD models to wp(rp) data',
+      url='https://bitbucket.org/parnalte/hod-fit',
+      author='Pablo Arnalte-Mur',
+      author_email='pablo.arnalte@uv.es',
+      license='BSD',
+      packages=['hodfit'],
+      zip_safe=False)
