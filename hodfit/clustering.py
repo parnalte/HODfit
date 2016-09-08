Bunch of edits trying to fix the 2-halo term calculation. Seems to be working when using enough precision in logM

hodfit/clustering.py
@@ -225,9 +225,19 @@ def mlim_nprime_tinker(rscales, redshift=0, cosmo=ac.WMAP7, hod_instance=None,
                                            halo_instance=halo_instance)
     
     mlim_indices = np.searchsorted(densgal_masslim, n_prime_out)
+    # For the cases when we get out of bounds, take the last value of the mass
+    mlim_indices[mlim_indices == hod_instance.Nm] = hod_instance.Nm - 1
     mass_lim_out = hod_instance.mass_array[mlim_indices]
     
-    return mass_lim_out, n_prime_out
+    # And now, compute the corresponding n_prime
+    n_prime_array = np.empty(Nr, float)
+
+    for i, mlim in enumerate(mass_lim_out):
+        n_prime_array[i] = \
+            hodmodel.dens_galaxies_arrays(hod_instance=hod_instance,
+                                          halo_instance=halo_instance,
+                                          mass_limit=mlim)
+    return mass_lim_out, n_prime_array
     
     
 
@@ -623,7 +633,7 @@ class HODClustering():
              
         # Finally, re-escale the xiprime we obtained following eq. (B9)
         # of Tinker-2005
-        xir_2h = (pow(nprimevals/self.gal_dens, 2)*(1. + xiprime)) - 1.
+        xir_2h = (pow(nprimevals/self.gal_dens, 1)*(1. + xiprime)) - 1.
         
         return xir_2h
         

hodfit/hodmodel.py
@@ -424,13 +424,17 @@ def probability_nonoverlap(radius, mass_1, mass_2, redshift=0, cosmo=ac.WMAP7):
     rvir_1 = densprofile.rvir_from_mass(mass_1, redshift, cosmo)
     rvir_2 = densprofile.rvir_from_mass(mass_2, redshift, cosmo)
 
-    rvirmesh_1, rvirmesh_2 = np.meshgrid(rvir_1, rvir_2, indexing='ij')
+    # rvirmesh_1, rvirmesh_2 = np.meshgrid(rvir_1, rvir_2, indexing='ij')
 
     # xvar, yvar are already N1xN2 arrays
-    xvar = radius/(rvirmesh_1 + rvirmesh_2)
+
+    # Use notation to create N1xN2 array from
+    # http://stackoverflow.com/a/20677444
+    xvar = radius/(rvir_1[:,None] + rvir_2)
     yvar = (xvar - 0.8)/0.29
 
-    result = 3*pow(yvar, 2) - 2*pow(yvar, 3)
+    #result = 3*pow(yvar, 2) - 2*pow(yvar, 3)
+    result = 3*yvar*yvar - 2*yvar*yvar*yvar
     result[yvar < 0] = 0
     result[yvar > 1] = 1
 
@@ -457,14 +461,15 @@ def galdens_haloexclusion(radius, redshift=0, cosmo=ac.WMAP7,
     
     dens_ntot_1d = hod_instance.n_tot_array*halo_instance.ndens_diff_m_array
     
-    dens_ntot_1, dens_ntot_2 = np.meshgrid(dens_ntot_1d, dens_ntot_1d)
+    # dens_ntot_1, dens_ntot_2 = np.meshgrid(dens_ntot_1d, dens_ntot_1d)
     
     prob_over_term = probability_nonoverlap(radius=radius,
                                             mass_1 = hod_instance.mass_array, 
                                             mass_2 = hod_instance.mass_array,
                                             redshift=redshift, cosmo=cosmo)
     
-    integrand_2d = dens_ntot_1*dens_ntot_2*prob_over_term
+    #integrand_2d = dens_ntot_1*dens_ntot_2*prob_over_term
+    integrand_2d = np.outer(dens_ntot_1d, dens_ntot_1d)*prob_over_term
     
     # Do the 2D integral by using Simpson's rule twice, as shown in
     # http://stackoverflow.com/a/20677444
@@ -496,11 +501,18 @@ def dens_galaxies_varmasslim(hod_instance=None, halo_instance=None):
     assert hod_instance.Nm == halo_instance.Nm
     assert (hod_instance.mass_array == halo_instance.mass_array).all()
     
-    integrand = hod_instance.n_tot_array*halo_instance.ndens_diff_m_array
+    #integrand = hod_instance.n_tot_array*halo_instance.ndens_diff_m_array
     
-    dens_gals_masslim = integrate.cumtrapz(y=integrand,
-                                           x=hod_instance.mass_array,
-                                           initial=0)
+    #dens_gals_masslim = integrate.cumtrapz(y=integrand,
+    #                                       x=hod_instance.mass_array,
+    #                                       initial=0)
+    
+    dens_gals_masslim = np.empty(hod_instance.Nm, float)
+    
+    for i,mlim in enumerate(hod_instance.mass_array):
+        dens_gals_masslim[i] = dens_galaxies_arrays(hod_instance=hod_instance,
+                                                    halo_instance=halo_instance,
+                                                    mass_limit=mlim)
                                            
     return dens_gals_masslim                                           
                                            

hodfit/test/manual_test_comparison.py
@@ -5,7 +5,7 @@ from hodfit import clustering as hc
 import numpy as np
 import pylab as pl
 
-hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762)
+hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, logM_step=0.01)
 
 rp = np.logspace(-2,2,30)
 
@@ -13,26 +13,26 @@ print "Computing default model"
 wp = hc.get_wptotal(rpvals=rp, clustering_object=hcobj)
 
 #With constant bias
-hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, scale_dep_bias=False)
+hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, logM_step=0.01, scale_dep_bias=False)
 
 print "Computing constant bias model"
 wp_cbias = hc.get_wptotal(rpvals=rp, clustering_object=hcobj)
 
 #Without Mvir limit in cs term
-hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, use_mvir_limit=False)
+hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, logM_step=0.01, use_mvir_limit=False)
 
 print "Computing no-Mvir-lim model"
 wp_nomvirlim = hc.get_wptotal(rpvals=rp, clustering_object=hcobj)
 
 #Without halo exclusion in 2h term
-hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, halo_exclusion_model=0)
+hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, logM_step=0.01, halo_exclusion_model=0)
 
 print "Computing no halo exclusion model"
 wp_nohaloexcl = hc.get_wptotal(rpvals=rp, clustering_object=hcobj)
 
 
 #With original MoWhite bias parameters
-hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, use_tinker_bias_params=False)
+hcobj = hc.hod_from_parameters(redshift=0.496412, OmegaM0=0.27, OmegaL0=0.73, powesp_linz0_file="WMAP7_linz0_matterpower.dat", powesp_matter_file="WMAP7_z0p52_matterpower.dat", hod_type=2, hod_mass_min=10**11.906, hod_mass_1=10**13.285, hod_alpha=1.4091, hod_siglogM=0.48456, hod_mass_0=10**6.6762, logM_step=0.01, use_tinker_bias_params=False)
 
 print "Computing original bias model"
 wp_origbias = hc.get_wptotal(rpvals=rp, clustering_object=hcobj)