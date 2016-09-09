74cf59c771a6e5f53f940134a644bcc12c028c3e
Pablo Arnalte-Mur <pablo.arnalte@gmail.com>
2016-09-09 20:18:20 +0200

    Change to make the speed test a bit more realistic

--- hodfit/test/speed_test.py
+++ hodfit/test/speed_test.py
@@ -16,7 +16,7 @@ pkfile_matter_z = "WMAP7_z0_matterpower.dat"
 hodclust_object = hc.hod_from_parameters(redshift=redshift, OmegaM0=OmegaM_0,
                                          OmegaL0=OmegaL_0, powesp_matter_file=pkfile_matter_z,
                                          powesp_linz0_file=pkfile_lin_z0, logM_min=6.,
-                                         logM_max=17., logM_step=0.1)
+                                         logM_max=17., logM_step=0.01)
 
 
 NRCALC = 100
@@ -43,7 +43,7 @@ print "Computing wp took ", t2 - t1, " seconds."
 hodclust_object = hc.hod_from_parameters(redshift=redshift, OmegaM0=OmegaM_0,
                                          OmegaL0=OmegaL_0, powesp_matter_file=pkfile_matter_z,
                                          powesp_linz0_file=pkfile_lin_z0, logM_min=6.,
-                                         logM_max=17., logM_step=0.1, halo_exclusion_model=0)
+                                         logM_max=17., logM_step=0.01, halo_exclusion_model=0)
 
 t1 = time.time()
 wp_start = wp_hod(rp=rp, log10Mmin=logMmin_start, log10M1=logM1_start, alpha=alpha_start, hodclustering=hodclust_object)
