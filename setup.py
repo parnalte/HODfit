c547afa3f54d2ff900c6a3c1b6675c9fdc59ac96
Pablo Arnalte-Mur <pablo.arnalte@gmail.com>
2022-04-13 18:03:12 +0200

    Update version no. (to dev)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ TODO:
 from setuptools import setup
 
 setup(name='hodfit',
-      version='0.18',
+      version='0.19dev',
       description='Fit HOD models to wp(rp) data',
       url='https://github.com/parnalte/HODfit',
       author='Pablo Arnalte-Mur',
